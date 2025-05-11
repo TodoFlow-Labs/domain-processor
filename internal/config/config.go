7a0882240d25f56866f8236c75afd4dd9d72e111
lieranderl <19622412+lieranderl@users.noreply.github.com>
2025-05-11 13:45:30 +0200

    update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -46,5 +46,13 @@ func Load() (*Config, error) {
 		return nil, err
 	}
 
+	// Validate
+	if cfg.NATSURL == "" {
+		return nil, fmt.Errorf("nats-url must be set")
+	}
+	if cfg.DatabaseURL == "" {
+		return nil, fmt.Errorf("database-url must be set")
+	}
+
 	return &cfg, nil
 }
